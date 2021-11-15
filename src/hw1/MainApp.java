7345909c760a5317300cb349b9bd1aaa45f7a172
FF <5kla@mail.ru>
2021-11-16 00:24:25 +0500

    hw1

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -2,6 +2,10 @@ package hw1;
 
 public class MainApp {
     public static void main(String[] args) {
+        test1();
+    }
 
+    public static void test1() {
+        System.out.println("test1");
     }
 }
