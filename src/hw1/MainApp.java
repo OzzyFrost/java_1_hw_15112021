95f3c1ddb2540deba3c5d439e8d29a57eb91c18e
FF <5kla@mail.ru>
2021-11-16 00:29:59 +0500

    hw1 доделал 2-й метод

--- src/hw1/MainApp.java
+++ src/hw1/MainApp.java
@@ -3,9 +3,14 @@ package hw1;
 public class MainApp {
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     public static void test1() {
+        System.out.println("test1");
+    }
+
+    public static void test2() {
         System.out.println("test1");
     }
 }
